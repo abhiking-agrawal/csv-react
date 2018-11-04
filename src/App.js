6e2d531565098fd4bbb95da6d933ae2a6663020c
abhi <s530670@nwmissouri.edu>
2018-11-03 20:11:27 -0500

    Project commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from "./Layout/layout";
+
+class App extends Component {
+  render() {
+    return (
+      <Layout></Layout>
+    );
+  }
+}
+
+export default App;
